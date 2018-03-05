5fc8d9566d033294dacb0ea7310c405b2a5f585f
gglavan <gicu.glavan@gmail.com>
2018-03-05 17:53:42 +0200

    eslint implemented

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var a = document.getElementById('btn');
-a.addEventListener("click",function(){
-  if(alert("click")){
-    alert("Ati apast OK")
+const a = document.getElementById('btn');
+a.addEventListener('click', () => {
+  if (alert('click')) {
+    alert('Ati apast OK');
   }
 });
